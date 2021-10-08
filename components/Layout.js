c77a6e43a73b33e4e8d39175a0c675182c5ecba1
sunday odum <sundayodum@gmail.com>
2021-10-08 14:58:10 +0100

    add material ui theme

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import Head from 'next/head';
 import NextLink from 'next/Link';
-import { AppBar, Toolbar, Typography, Container, Link } from '@material-ui/core';
+import { AppBar, Toolbar, Typography, Container, Link, createTheme, ThemeProvider, CssBaseline } from '@material-ui/core';
 import useStyles from '../utils/styles';
  
 
 export default function Layout({title, description, children }) {
+  const theme = createTheme({
+    typography: {
+      h1: {
+        fontSize: '1.6rem',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+      h2: {
+        fontSize: '1.4rem',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+    },
+    palette: {
+      type: 'light',
+      primary: {
+        main: '#f0c000',
+      },
+      secondary: {
+        main: '#208080',
+      },
+    },
+  });
   const classes = useStyles(); 
     return (
     <div>
@@ -13,6 +36,8 @@ export default function Layout({title, description, children }) {
         <title>{title? `${title} - Next Buyhub`: 'Next Amazona'}</title>
         {description && <meta name="description" content={description}></meta>}
       </Head>
+      <ThemeProvider theme={theme}>
+         <CssBaseline/>
       <AppBar position="static" className={classes.navbar}>
         <Toolbar>
             <NextLink href="/" passHref>
@@ -37,6 +62,7 @@ export default function Layout({title, description, children }) {
       <footer className={classes.footer}>
         <Typography>All rights reserved. Next Buyhub.</Typography>
       </footer>
+      </ThemeProvider>
     </div>
   );
 }
